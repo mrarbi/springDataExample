766f396591776bc073a39f93edac371d315e2ae2
[a606142] mouad arbi <mouad.arbi@atos.net>
2016-01-26 17:22:28 +0000

    cleaning repo

--- springDataExample/src/main/java/auto/from/spring/data/example/PersonRepository.java
+++ springDataExample/src/main/java/auto/from/spring/data/example/PersonRepository.java
@@ -3,6 +3,7 @@ package auto.from.spring.data.example;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
 	
